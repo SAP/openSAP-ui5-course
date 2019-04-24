df26847c4534d20b731816b0bafbbaecf59bd0f8
Michael Graf <mi.graf@sap.com>
2019-04-24 16:03:43 +0200

    Purge dummy content for video recording

--- demo/odatav4/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"sap/ui/demo/odataV4/test/unit/controller/App.controller"
-], function () {
-	"use strict";
-});
--- w2u3/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"opensap/movies/test/unit/controller/App.controller"
-], function () {
-	"use strict";
-});
