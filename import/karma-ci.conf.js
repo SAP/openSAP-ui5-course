b62fdf03bb3325edbf21f6c7bc66e4c4f30cbbd6
Arnd vom Hofe <a.vom.hofe@sap.com>
2019-05-08 08:29:57 +0200

    package.json and karma added to import folder

--- import/karma-ci.conf.js
+++ import/karma-ci.conf.js
@@ -9,39 +9,6 @@ module.exports = function(config) {
 
 	config.set({
 
-		preprocessors: {
-			'{webapp,webapp/!(test)}/*.js': ['coverage']
-		},
-
-		coverageReporter: {
-			includeAllSources: true,
-			reporters: [
-				{
-					type: 'html',
-					dir: '../coverage/'
-				},
-				{
-					type: 'text'
-				}
-			],
-			check: {
-				each: {
-					statements: 100,
-					branches: 100,
-					functions: 100,
-					lines: 100
-				}
-			}
-		},
-
-		client: {
-			qunit: {
-				showUI: false
-			}
-		},
-
-		reporters: ['progress', 'coverage'],
-
 		browsers: ['ChromeHeadless'],
 
 		singleRun: true
